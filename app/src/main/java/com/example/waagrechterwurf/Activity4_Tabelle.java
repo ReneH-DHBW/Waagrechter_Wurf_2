b0eb2a5c7ce703b52c2b8e5436e375e3ff0a1fc1
Jannik Steck <jannik.steck@gmail.com>
2019-10-23 21:20:45 +0200

    Schau dir mal das Layout an. Sieht finde ich nicht schlecht aus

--- app/src/main/java/com/example/waagrechterwurf/Activity4_Tabelle.java
+++ app/src/main/java/com/example/waagrechterwurf/Activity4_Tabelle.java
@@ -5,10 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.List;
 
